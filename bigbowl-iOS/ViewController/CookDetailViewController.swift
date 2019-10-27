f8e82586c462e2748db3f14d7e6453ea13c9ee82
Philip Chwistek <mrsportsnerd@gmail.com>
2019-10-26 22:47:06 -0700

    added cookdetail

--- bigbowl-iOS/ViewController/CookDetailViewController.swift
+++ bigbowl-iOS/ViewController/CookDetailViewController.swift
@@ -9,6 +9,54 @@
 import Foundation
 import UIKit
 
+class MenuCell: UITableViewCell {
+    
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var stars: UILabel!
+    @IBOutlet weak var meal: UILabel!
+    @IBOutlet weak var foodImage: UIImageView!
+    
+}
+
+
 class CookDetailViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+}
+
+extension CookDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as! MenuCell
+        
+        // let item = self.items[indexPath.item]
+        cell.meal?.text = "Pad thai"
+        cell.stars?.text = "5/5 rating"
+        cell.price?.text = "$5.99"
+//        cell.cookImage?.image = UIImage(named: "flame")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Featured"
+    }
+}
+
+extension CookDetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("hello")
+        if let viewController = storyboard?.instantiateViewController(identifier: "FoodDetailViewController") as? FoodDetailViewController {
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
 }
