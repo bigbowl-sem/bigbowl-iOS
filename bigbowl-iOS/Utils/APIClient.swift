17ca4044159023bc5b7ece92cd53340ed2acd4f0
Philip Chwistek <mrsportsnerd@gmail.com>
2019-11-10 22:27:59 -0800

    added changes

--- bigbowl-iOS/Utils/APIClient.swift
+++ bigbowl-iOS/Utils/APIClient.swift
@@ -248,5 +248,69 @@ class APIClient: NSObject {
               }
     }
     
+      //Store authentication
+      func postAccount(accountId: String,email: String, password: String,firstName:String,lastName: String,phone: String,isEater: Bool, isCook: Bool, completionHandler: @escaping (DataResponse<String>?, Error?) -> Void)  {
+            let url = baseURL.appendingPathComponent("account")
+          print (url)
+            var parameters = [String:Any]()
+            parameters["accountId"] = accountId
+            parameters["email"] = email
+            parameters["password"] = password
+            parameters["firstName"] = firstName
+            parameters["lastName"] = lastName
+            parameters["phone"] = phone
+            parameters["isEater"] = isEater
+            parameters["isCook"] = isCook
+           print (accountId ,email)
+          
+          var request = URLRequest(url: url)
+          request.httpMethod = "POST"
+          request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+          guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+          request.httpBody = httpBody
+          
+          let session = URLSession.shared
+          session.dataTask(with: request) { (data, response, error) in
+              if let response = response {
+                  print(response)
+              }
+              
+              if let data = data {
+                  do {
+                      let json = try JSONSerialization.jsonObject(with: data, options: [])
+                      print(json)
+                  } catch {
+                      print(error)
+                  }
+              }
+              
+          }.resume()
+          
+        }
+     
+    func getAccount(accountId: String, completionHandler: @escaping (DataResponse<String>?, Error?) -> Void)  {
+          let url = baseURL.appendingPathComponent("account" + "/" + accountId)
+          /*var parameters = [String:Any]()
+          parameters["lng"] = coordinates.coordinate.longitude
+          parameters["lat"] = coordinates.coordinate.latitude
+          parameters["radius"] = 1*/
+          
+          Alamofire.request(url, method: .get)
+              .validate(statusCode: 200..<300)
+              .responseString { response in
+                  switch response.result {
+                      case .success:
+                          print(response)
+                      completionHandler(response, nil)
+                      break
+                      case .failure(let error):
+                      print(error)
+                      completionHandler(nil, error)
+                      break
+                  }
+          }
+      }
+      
+    
     
 }
